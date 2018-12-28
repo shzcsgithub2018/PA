33667152a0f254a5ffddb3af194d1eb575d2619c
tracer-ics2018 <tracer@njuics.org>
2018-12-28 08:17:03 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:17:03 up 1 day, 11 min,  1 user,  load average: 0.71, 0.86, 0.94 aa8ce03bc858b539431a57ef8bd14b9c38d1abda

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -52,7 +52,7 @@ static struct {
     { "help", "Display informations about all supported commands", cmd_help },
     { "c", "Continue the execution of the program", cmd_c },
     { "q", "Exit NEMU", cmd_q },
-    {"si [Num]","Step Num instructions or step one instructions",cmd_si},
+    { "si [Num]","Step Num instructions or step one instructions",cmd_si},
     /* TODO: Add more commands */
 
 };
