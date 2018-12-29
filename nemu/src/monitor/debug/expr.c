c8398d0c1acf5478ddfb164c7977a7cf030c5b62
tracer-ics2018 <tracer@njuics.org>
2018-12-29 14:47:19 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:47:19 up 2 days,  6:41,  3 users,  load average: 0.75, 0.51, 0.55 b09241f4bcc78e84176f350f6e97ddbdad7dba83

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -80,7 +80,8 @@ static bool make_token(char *e) {
 
                 Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
                         i, rules[i].regex, position, substr_len, substr_len, substr_start);
-                        
+                Log("nr_token=%d",nr_token);
+				
                 position += substr_len;
 
                 /* TODO: Now a new token is recognized with rules[i]. Add codes
