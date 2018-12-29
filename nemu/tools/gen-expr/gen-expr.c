492a99aac334e7eb346598c72cb2116d3c4faeaf
tracer-ics2018 <tracer@njuics.org>
2018-12-29 10:30:09 +0800

    >  run U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:30:09 up 2 days,  2:24,  3 users,  load average: 0.47, 0.58, 0.55 41467814a5d7d41d3ba92b4b997d7e11768cd897

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -6,28 +6,73 @@
 #include <string.h>
 
 // this should be enough
-static char buf[65536];
+#define Maxsize_buf 65536
+#define Convergence_condition (Maxsize_buf/4/6)
+static char buf[Maxsize_buf];
+static int p_buf;
 
-uint32_t choose(uint32_t n){
-  strand(time(0));
-  n=rand()%3;
-  return n;
+static inline void int_toString(int n,char *str){
+  char s[6];
+  int p_s=0;
+
+  while(n-->0){
+      s[p_s++]=n%10+'0';
+      n/=10;
+  }
+
+  for(int i=0;i<p_s;i++)
+    str[i]=s[p_s-1-i];
+  str[p_s]='\0';
 }
 
 static inline void gen_rand_expr() {
-   switch (choose(3)) {
-    case 0: gen_num(); break;
-    case 1: gen('('); gen_rand_expr(); gen(')'); break;
-    default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+   int rand_num=rand()%3;
+   if(p_buf>=Convergence_condition)
+      rand_num=0;
+
+   switch (rand_num) {
+    case 0: {
+      int num=1+rand()%10000;//Don't allow 0;
+      char to_str[6];
+      int_toString(num,to_str); 
+      for(int i=0;i<strlen(to_str);i++)
+        buf[p_buf++]=to_str[i];
+      break;
+    }
+    case 1: {
+      buf[p_buf++]='(';
+      gen_rand_expr(); 
+      buf[p_buf++]=')';
+      break;
+    }
+    default: {
+      gen_rand_expr(); 
+      switch(rand()%4){
+        case 0:
+          buf[p_buf++]='+';
+          break;
+        case 1:
+          buf[p_buf++]='-';
+          break;
+        case 2:
+          buf[p_buf++]='*';
+          break;
+        default:
+          buf[p_buf++]='/';
+          break;
+      }
+      gen_rand_expr(); 
+      break;
+    }
   }
-  buf[0] = '\0';
+  buf[p_buf] = '\0';
 }
 
 static char code_buf[65536];
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned result = %s; "
+"  unsigned result = %s ;"
 "  printf(\"%%u\", result); "
 "  return 0; "
 "}";
@@ -43,6 +88,7 @@ int main(int argc, char *argv[]) {
 
   int i;
   for (i = 0; i < loop; i ++) {
+    p_buf=0;
     gen_rand_expr();
 
     sprintf(code_buf, code_format, buf);
