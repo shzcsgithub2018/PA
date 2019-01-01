b121a6baa941a9899c80030f53a3759c64acfdc7
tracer-ics2018 <tracer@njuics.org>
2019-01-01 10:08:31 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:08:31 up 5 days,  2:03,  1 user,  load average: 0.56, 0.62, 0.70 9a4296dbec54e554593c023c1f5eeb2d42ba66d

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -26,7 +26,7 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  rtl_push(&decoding.seq_eip);
+  rtl_push(&cpu.eip);
   rtl_j(decoding.jmp_eip);
 
   print_asm("call %x", decoding.jmp_eip);
