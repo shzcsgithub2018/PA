6417593f464bfdf1bd8c9811eb1391259aef410e
tracer-ics2018 <tracer@njuics.org>
2019-01-01 20:29:55 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:29:55 up 5 days, 12:24,  1 user,  load average: 0.35, 0.44, 0.53 1388147580ee78d680bb5b259ae6fa83cf85aec3

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -55,6 +55,8 @@ static inline int check_reg_index(int index) {
     return index;
 }
 
+#define eflages(f)  concat(cpu.eflages.,f)
+
 #define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
 #define reg_w(index) (cpu.gpr[check_reg_index(index)]._16)
 #define reg_b(index) (cpu.gpr[check_reg_index(index) & 0x3]._8[index >> 2])
