c5dcfa5326dbd8ad97bd66f366c40e05cd383221
tracer-ics2018 <tracer@njuics.org>
2019-01-02 20:57:05 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:57:05 up  3:55,  1 user,  load average: 0.47, 0.46, 0.56 a90463ea5c3d2587ef0a20c0ab591a72f8340d36

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -240,8 +240,9 @@ void exec_wrapper(bool print_flag) {
 
   decoding.seq_eip = ori_eip;
   exec_real(&decoding.seq_eip);
-  Log("fasf");
+
 #ifdef DEBUG
+  Log("fasf");
   int instr_len = decoding.seq_eip - ori_eip;
   sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
   strcat(decoding.asm_buf, decoding.assembly);
@@ -250,7 +251,7 @@ void exec_wrapper(bool print_flag) {
     puts(decoding.asm_buf);
   }
 #endif
-  Log("fasf");
+
   update_eip();
 
 #if defined(DIFF_TEST)
