1f305b636eda0fbef045fc7e1a3e96fa423bb8e9
tracer-ics2018 <tracer@njuics.org>
2019-01-02 16:26:41 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:26:41 up 6 days,  8:21,  1 user,  load average: 0.64, 0.73, 0.63 940611f9a88a70c31bc201815fef053a4b50a5e1

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -155,7 +155,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  Log("左移8位:0x%x",~(uint32_t)0>>(width*8));
+  Log("左移8位:0x%x",~(uint32_t)0<<(width*8));
   *dest=(~0u<<(width*8))|*src1;
 }
 
