b31c50b55d067170c966c980fa021c10c78635ea
tracer-ics2018 <tracer@njuics.org>
2019-01-02 15:22:54 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:22:54 up 6 days,  7:17,  1 user,  load average: 0.57, 0.53, 0.54 2dea38939662bb5ff9497633ef650e1c9242a28a

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -71,7 +71,7 @@ make_EHelper(movzx) {
 }
 
 make_EHelper(lea) {
-  Log("ecx=x%x",cpu.ecx);
+  Log("ecx=x%x  0x4(esp)=0x%x",cpu.ecx,vaddr_read(cpu.esp,4));
   operand_write(id_dest, &id_src->addr);
   Log("ecx=x%x",cpu.ecx);
   print_asm_template2(lea);
