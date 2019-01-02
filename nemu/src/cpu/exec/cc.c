591f6a18aa5e9c595778b54c6bf35aa43de1464a
tracer-ics2018 <tracer@njuics.org>
2019-01-02 10:13:23 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:13:23 up 6 days,  2:08,  1 user,  load average: 0.74, 0.81, 0.80 dd0803834b6e2cf58bef4124c598b7ebcfeb74f9

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,13 +15,26 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+      *dest=cpu.eflages.OF?1:0;
+      break;
     case CC_B:
+      *dest=cpu.eflages.CF?1:0;
+      break;
     case CC_E:
+      *dest=cpu.eflages.ZF?1:0;
+      break;
     case CC_BE:
+      *dest=(cpu.eflages.CF||cpu.eflages.ZF)?1:0;
+      break;
     case CC_S:
+      *dest=cpu.eflages.SF?1:0;
+      break;
     case CC_L:
+      *dest=(cpu.eflages.SF!=cpu.eflages.OF)?1:0;
+      break;
     case CC_LE:
-      TODO();
+      *dest=((cpu.eflages.SF!=cpu.eflages.OF)||cpu.eflages.ZF)?1:0;
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
