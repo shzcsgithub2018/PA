a550a5c68d2a52ba050f3aaf81c1dcd75b6f4eb9
tracer-ics2018 <tracer@njuics.org>
2019-01-03 08:31:56 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  08:31:56 up 15:30,  1 user,  load average: 0.81, 0.88, 0.84 ac2fd0e84198391ccf28b4e88ac738625116ffb

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -41,8 +41,8 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   op->simm = instr_fetch(eip, op->width);
-  rtl_li(&op->val,op->simm);
-  // rtl_sext(&op->val,&at,op->width);
+  rtl_li(&at,op->simm);
+  rtl_sext(&op->val,&at,op->width);
 #ifdef DEBUG
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
 #endif
