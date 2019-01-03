5920fc8daa9b5256c4f6145ea172366e7cedf531
tracer-ics2018 <tracer@njuics.org>
2019-01-03 11:37:46 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:37:46 up 18:35,  1 user,  load average: 0.90, 0.77, 0.56 464e7b3221fcc9d83a3e286661581a36d1fd0aa2

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -5,9 +5,13 @@ make_EHelper(add) {
   // Log("fasfdfaff");
   rtl_add(&id_dest->val,&id_dest->val,&id_src->val);
   operand_write(id_dest,&id_dest->val);
-
-  rtl_update_ZF(&id_dest->val,id_dest->width);
+  
   // Log("ZF=%d",cpu.eflages.ZF);
+  if(id_dest->val==0)
+    rtl_li(&t1, 1);
+  else
+    rtl_li(&t1, 0);
+  rtl_set_ZF(&t1);
   // Log("ZF=%d",cpu.eflages.ZF);
   // Log("ebx=0x%x",cpu.ebx);
   print_asm_template2(add);
@@ -20,7 +24,11 @@ make_EHelper(sub) {
   rtl_sub(&id_dest->val,&id_dest->val,&id_src->val);
   operand_write(id_dest,&id_dest->val);
 
-  rtl_update_ZF(&id_dest->val,id_dest->width);
+  if(id_dest->val==0)
+    rtl_li(&t1, 1);
+  else
+    rtl_li(&t1, 0);
+  rtl_set_ZF(&t1);
   // Log("dest= 0x%x src=0x%x\n",id_dest->val,id_src->val);
   // Log("sub   esp=0x%x\n",cpu.esp);
   print_asm_template2(sub);
@@ -31,7 +39,11 @@ make_EHelper(cmp) {
   rtl_sext(&t0,&id_src->val,id_dest->width);
   rtl_sub(&t0,&id_dest->val,&id_src->val);
 
-  rtl_update_ZF(&id_dest->val,id_dest->width);
+  if(t0==0)
+    rtl_li(&t1, 1);
+  else
+    rtl_li(&t1, 0);
+  rtl_set_ZF(&t1);
 
   print_asm_template2(cmp);
 }
--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -9,7 +9,9 @@ make_EHelper(test) {
   rtl_set_CF(&t1);
   rtl_set_OF(&t1);
   // Log("ZF=%d",cpu.eflages.ZF);
-  rtl_update_ZF(&id_dest->val,id_dest->width);
+  if(t0==0)
+    rtl_li(&t1, 1);
+  rtl_set_ZF(&t1);
 
   //  Log("ZF=%d",cpu.eflages.ZF);
   print_asm_template2(test);
@@ -28,7 +30,11 @@ make_EHelper(and) {
 
   operand_write(id_dest, &id_dest->val);
 
-  rtl_update_ZF(&id_dest->val,id_dest->width);
+  if(id_dest->val==0)
+    rtl_li(&t1, 1);
+  else
+    rtl_li(&t1, 0);
+  rtl_set_ZF(&t1);
   // Log("esp=0x%x\n",cpu.esp);
   // Log("dest= 0x%x src=0x%x\n",id_dest->val,id_src->val);
 
@@ -47,7 +53,9 @@ make_EHelper(xor) {
   rtl_set_CF(&t0);
   rtl_set_OF(&t0);
 
-  rtl_update_ZF(&id_dest->val,id_dest->width);
+  if(id_dest->val==0)
+    rtl_li(&t0,1);
+  rtl_set_ZF(&t0);
 
   print_asm_template2(xor);
 }
@@ -63,7 +71,9 @@ make_EHelper(or) {
   rtl_set_CF(&t0);
   rtl_set_OF(&t0);
 
-  rtl_update_ZF(&id_dest->val,id_dest->width);
+  if(id_dest->val==0)
+    rtl_li(&t0,1);
+  rtl_set_ZF(&t0);
   // Log("ZF=0x%x",cpu.eflages.ZF);
   // Log("dest->val=0x%x   src->val=0x%x",id_dest->val,id_src->val);
   // Log("edx=0x%x   eax=0x%x\n",cpu.edx,cpu.eax);
