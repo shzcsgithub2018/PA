792286afeac8ffb2c5e3aa138d9fd71027b006f6
tracer-ics2018 <tracer@njuics.org>
2019-01-04 16:24:45 +0800

    >  run U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:24:45 up 1 day, 13 min,  1 user,  load average: 1.21, 0.89, 0.86 f43c55b51eddefaa2ca8fdfbb386f9e0ec51eab3

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -14,7 +14,16 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  if(src==NULL)
+    return NULL;
+
+  char *dst_itor=dst;
+
+  do{
+    *dst_itor++=*src;
+  }while(*src!=0);
+
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
@@ -22,10 +31,21 @@ char* strncpy(char* dst, const char* src, size_t n) {
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  if(src==NULL)
+    return dst;
+  
+  char *dst_itor=dst;
+  while(*dst_itor!=0)dst_itor++;
+  while(*src!=0){
+    *dst_itor++=*src++;
+  }
+  *dst_itor=0;
+
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
+
   if(s1==NULL&&s2==NULL)
     return 0;
   if(s1==NULL)return -1;
@@ -51,6 +71,14 @@ void* memset(void* s,int c,size_t n) {
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+  // if(!n)
+  //   return 0;
+
+  // while(--n ){
+  //   && *(char *)s1 == *(char *)s2
+  //   s1 = (char *)s1 + 1;
+  //   s2 = (char *)s2 + 1;
+  // }
   return NULL;
 }
 
