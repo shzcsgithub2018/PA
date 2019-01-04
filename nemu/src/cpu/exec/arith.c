c6e092a74430ed0bc11f615def9f4438a0736b43
tracer-ics2018 <tracer@njuics.org>
2019-01-04 10:55:31 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:55:31 up 18:44,  1 user,  load average: 0.36, 0.54, 0.59 c07984f8276d6200a70924cdd4c280a464abfec

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -36,12 +36,14 @@ make_EHelper(sub) {
   t3=rtl_get_sign(&t0,id_dest->width);
   at=rtl_get_sign(&id_dest->val,id_dest->width);
   t2=rtl_get_sign(&t1,id_dest->width);
-
-  if((at&&!t3)||t2)
+  // Log("result=0x%x   id_dest->val=0x%x id_src->val=0x%x",t1,id_dest->val,id_src->val);
+  // Log("sign:  src=0x%x  dest=0x%x   t1=0x%x",t3,at,t2);
+  if((!at&&t3)||t2)
     rtl_li(&t2,1);
   else
     rtl_li(&t2,0);
   rtl_set_CF(&t2);
+  Log("CF=%d",cpu.eflages.CF);
   // Log("dest= 0x%x src=0x%x\n",id_dest->val,id_src->val);
   // Log("sub   esp=0x%x\n",cpu.esp);
   print_asm_template2(sub);
