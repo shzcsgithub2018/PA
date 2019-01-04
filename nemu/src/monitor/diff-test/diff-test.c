77e203b42dfc0289025183d4c5eac8991a6470dd
tracer-ics2018 <tracer@njuics.org>
2019-01-04 17:33:22 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:33:22 up 1 day,  1:21,  1 user,  load average: 0.84, 0.74, 0.69 a497dc3f36310729667963bdef257678c8152f12

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -73,11 +73,12 @@ void difftest_step(uint32_t eip) {
   // Set `nemu_state` to `NEMU_ABORT` if they are not the same.
   // TODO();
   for(int i=0;i<8;i++){
-    if(cpu.gpr[i]._32!=ref_r.gpr[i]._32){
-        nemu_state=NEMU_ABORT;
-    }
-    printf("hhhhhhh\n");
+    // if(cpu.gpr[i]._32!=ref_r.gpr[i]._32){
+    //     nemu_state=NEMU_ABORT;
+    // }
+    printf("ref_r.gpr[i]._32=0x%x\n",ref_r.gpr[i]._32);
   }
+  printf("ref_r.eip=0x%x\n",ref_r.eip);
   // if(cpu.eip!=ref_r.eip){
   //   printf("0x%x   0x%x\n",cpu.eip,ref_r.eip);
   //   nemu_state=NEMU_ABORT;
