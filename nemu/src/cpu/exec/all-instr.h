2631b245f8fafe07747165038288c68747c2f2da
tracer-ics2018 <tracer@njuics.org>
2019-01-04 09:13:10 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  09:13:10 up 17:01,  1 user,  load average: 0.73, 0.83, 0.80 caed7f9f713082f0b45ce48bddaa3b5fcb43528

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -45,6 +45,7 @@ make_EHelper(cmp_a);
 
 make_EHelper(sar);
 make_EHelper(shl);
+make_EHelper(shr);
 make_EHelper(not);
 
 make_EHelper(dec);
--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -88,9 +88,10 @@ make_EHelper(shl) {
 }
 
 make_EHelper(shr) {
-  TODO();
+  // TODO();
   // unnecessary to update CF and OF in NEMU
-
+  rtl_shri(&id_dest->val,&id_dest->val,id_src->val);
+  operand_write(id_dest,&id_dest->val);
   print_asm_template2(shr);
 }
 
