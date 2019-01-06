b053c0ade0fbeac2f185f51839fbf3d70796d75d
tracer-ics2018 <tracer@njuics.org>
2019-01-06 21:00:46 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:00:46 up 23:21,  1 user,  load average: 1.03, 0.79, 0.77 9e49d209b36d72fc3688f8f5bee7e807d344aff6

--- nexus-am/am/arch/native/src/dev/video.c
+++ nexus-am/am/arch/native/src/dev/video.c
@@ -57,6 +57,9 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
+      int i;
+      int size = screen_width() * screen_height();
+      for (i = 0; i < size; i ++) fb[i] = i;
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
       int cp_bytes = sizeof(uint32_t) * min(w, W - x);
