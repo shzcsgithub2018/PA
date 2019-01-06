a54fb5876ac4747e2da3f0b3ae65da7d832ece70
tracer-ics2018 <tracer@njuics.org>
2019-01-06 20:08:13 +0800

    >  run U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:08:13 up 22:29,  1 user,  load average: 0.79, 0.75, 0.77 a2e26da6291475e9e0d995002030624be1ab11b

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -2,12 +2,21 @@
 #include <x86.h>
 #include <amdev.h>
 
+#define I8042_DATA_PORT 0x60
+static int keydown=0;
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+      uint16_t keycode=inw(I8042_DATA_PORT);
+      if(keycode!=_KEY_NONE){
+          kbd->keydown=!keydown;
+          kbd->keycode=keycode;
+      }
+      else{
+        kbd->keydown = 0;
+        kbd->keycode = _KEY_NONE;
+      }
       return sizeof(_KbdReg);
     }
   }
