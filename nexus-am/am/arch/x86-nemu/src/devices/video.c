717d559c11e76b788dfffe622bc9731686c74269
tracer-ics2018 <tracer@njuics.org>
2019-01-06 21:27:09 +0800

    >  run U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  21:27:09 up 23:48,  1 user,  load average: 0.20, 0.49, 0.58 4accd47879e688f138240b327959415f2a646e

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -21,9 +21,16 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      int i;
-      int size = screen_width() * screen_height();
-      for (i = 0; i < size; i ++) fb[i] = i;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int min=w;
+      if(w>400-w)
+        min=400-w;
+      int cp_bytes = sizeof(uint32_t) * min;
+      for (int j = 0; j < h && y + j < 300; j ++) {
+        memcpy(&fb[(y + j) * 400 + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
