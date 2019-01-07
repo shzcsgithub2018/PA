afe1f9b0742171cdc9ab207dabb3e60e348c3b74
tracer-ics2018 <tracer@njuics.org>
2019-01-07 10:37:10 +0800

    >  run U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:37:10 up 1 day, 12:58,  1 user,  load average: 1.53, 1.03, 0.83 ae34e838a6110c886faec0c5f265e121cc60abcd

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -22,9 +22,11 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
       // int size = screen_width() * screen_height();
+      int w=screen_width();
+      // int h=screen_height();
       for(int j=ctl->y;j<=ctl->y+ctl->h;j++)
         for(int i=ctl->x;i<=ctl->x+ctl->w;i++)
-          fb[i*j]=*ctl->pixels;
+          fb[(j-1)*w+i]=*ctl->pixels;
         
       if (ctl->sync) {
         // do nothing, hardware syncs.
