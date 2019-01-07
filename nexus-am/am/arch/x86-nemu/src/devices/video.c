09722c44f7108931281d2055e4955a063453ed6d
tracer-ics2018 <tracer@njuics.org>
2019-01-07 10:58:14 +0800

    >  run U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  10:58:14 up 1 day, 13:19,  1 user,  load average: 0.89, 1.18, 1.19 479939662f2f2187304837b96c9a085d6b4f8cdc

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -22,14 +22,21 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
       // int size = screen_width() * screen_height();
-      int w=screen_width();
-      // int h=screen_height();
-      for(int j=ctl->y;j<=ctl->y+ctl->h;j++)
-        for(int i=ctl->x;i<=ctl->x+ctl->w;i++){
-          fb[(j-1)*w+i]=*ctl->pixels;
-          ctl->pixels+=ctl->w;
-        }
-        
+      int W=screen_width();
+      int H=screen_height();
+      // for(int j=ctl->y;j<=ctl->y+ctl->h;j++)
+      //   for(int i=ctl->x;i<=ctl->x+ctl->w;i++){
+      //     fb[(j-1)*w+i]=*ctl->pixels;
+      //     ctl->pixels+=ctl->w;
+      //   }
+      
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * (w>W-w?W-w:w);
+      for (int j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
