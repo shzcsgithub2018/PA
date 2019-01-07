c587fe086ef17372f781a86cd934f122dc0e47a6
tracer-ics2018 <tracer@njuics.org>
2019-01-07 11:08:28 +0800

    >  run U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:08:28 up 1 day, 13:29,  1 user,  load average: 0.99, 0.84, 0.99 85335df7b546c1bfa056236abda04e2624f1100f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -68,15 +68,31 @@ void* memset(void* s,int c,size_t n) {
   return s;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
-  if(!n)
-    return 0;
-  char *s1=out;
-  while(--n ){
-     *s1++=*(char *)in;
-      in=(char *)in+1;
-  }
-  return s1;
+void* memcpy(void* dst, const void* src, size_t len) {
+ 	if(NULL == dst || NULL == src){
+		return NULL;
+	}
+	
+	void *ret = dst;
+	
+	if(dst <= src || (char *)dst >= (char *)src + len){
+		//没有内存重叠，从低地址开始复制
+		while(len--){
+			*(char *)dst = *(char *)src;
+			dst = (char *)dst + 1;
+			src = (char *)src + 1;
+		}
+	}else{
+		//有内存重叠，从高地址开始复制
+		src = (char *)src + len - 1;
+		dst = (char *)dst + len - 1;
+		while(len--){
+			*(char *)dst = *(char *)src;
+			dst = (char *)dst - 1;
+			src = (char *)src - 1;
+		}
+	}
+	return ret;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
