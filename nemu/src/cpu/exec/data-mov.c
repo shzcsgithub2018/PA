530ea891d322858123f156288a35db3d20f217fa
tracer-ics2018 <tracer@njuics.org>
2019-01-08 19:09:59 +0800

    >  compile U201514765 shz1 Linux shz-Lenovo-Y50p-70 4.15.0-43-generic #46-Ubuntu SMP Thu Dec 6 14:45:28 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:09:58 up 1 day,  7:31,  1 user,  load average: 3.27, 2.33, 1.97 f7bccefd9e76f4b48a0d8ea569f4e54d76c25b78

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -8,7 +8,8 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  rtl_push(&id_dest->val);
+  rtl_sext(&t0,&id_dest->val,id_dest->width);
+  rtl_push(&t0);
   // Log("push 0x%x\n",id_dest->val);
   print_asm_template1(push);
 }
